f8737e984232c3a9fa93f91bc1099ae7eac1756f
avnibabun <avni.babun@ogr.gelisim.edu.tr>
2026-02-25 10:51:05 +0300

    Program.cs

--- CarProject/Program.cs
+++ CarProject/Program.cs
@@ -1,10 +1,16 @@
-﻿namespace CarProject
+namespace CarProject
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Car myCar = new Car("Toyota", "Red");
+
+            myCar.Accelerate(50);
+
+            Console.ReadLine();
+
+            myCar.Accelerate(10);
 
             Console.ReadLine();
         }
